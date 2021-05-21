eed73daa9e316ed2d4bbc0dab0b828c7fbfcb907
Craig Topper <craig.topper@sifive.com>
2021-05-21 14:12:01 -0700

    Update saxpy and sgemm examples to move the vsetvli into the loop body and not make the loop condition depend on it.
    vsetvl only returns 0 if the input is 0 so there's no need to test
    for 0 after the vsetvl. It does add more lines to C code, but generates
    shorter and probably faster assembly.

--- rvv_saxpy.c
+++ rvv_saxpy.c
@@ -54,7 +54,8 @@ void saxpy_vec(size_t n, const float a, const float *x, float *y) {
 
   vfloat32m8_t vx, vy;
 
-  for (; (l = vsetvl_e32m8(n)) > 0; n -= l) {
+  for (; n > 0; n -= l) {
+    l = vsetvl_e32m8(n);
     vx = vle32_v_f32m8(x, l);
     x += l;
     vy = vle32_v_f32m8(y, l);
