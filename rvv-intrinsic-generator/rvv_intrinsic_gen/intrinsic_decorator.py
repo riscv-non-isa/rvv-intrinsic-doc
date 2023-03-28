de82b8235007591e96a3c888650c4e51ea0d8c82
eopXD <yueh.ting.chen@gmail.com>
2023-03-28 02:29:05 -0700

    Improve segment load/store intrinsics to use tuple type
    Signed-off-by: eop Chen <eop.chen@sifive.com>

--- rvv-intrinsic-generator/rvv_intrinsic_gen/intrinsic_decorator.py
+++ rvv-intrinsic-generator/rvv_intrinsic_gen/intrinsic_decorator.py
@@ -57,7 +57,11 @@ class IntrinsicDecorator():
     if self.is_mask:
       g.write("// masked functions\n")
 
-  def mask_args(self, mask_type, maskoff_type=None, nf=None):
+  def mask_args(self,
+                mask_type,
+                maskoff_type=None,
+                nf=None,
+                is_seg_load_store_tuple_type=False):
     if self.is_mask and self.need_maskedoff:
       assert maskoff_type is not None
       d = collections.OrderedDict()
@@ -65,8 +69,11 @@ class IntrinsicDecorator():
       if nf is None:
         d["maskedoff"] = maskoff_type
       else:
-        for i in range(nf):
-          d[f"maskedoff{i}"] = maskoff_type
+        if is_seg_load_store_tuple_type:
+          d["maskedoff_tuple"] = f"{maskoff_type.split('_')[0]}x{nf}_t"
+        else:
+          for i in range(nf):
+            d[f"maskedoff{i}"] = maskoff_type
       return d
     elif self.is_mask:
       return {"mask": mask_type}
@@ -83,15 +90,21 @@ class IntrinsicDecorator():
     else:  # Implicit functions without NEED_MERGE and NEED_MASKED_OFF
       return {}
 
-  def tu_dest_args(self, dest_type, nf=None):
+  def tu_dest_args(self,
+                   dest_type,
+                   nf=None,
+                   is_seg_load_store_tuple_type=False):
     if self.has_maskedoff_name and not self.need_maskedoff:
       assert dest_type is not None
       d = collections.OrderedDict()
       if nf is None:
         d["maskedoff"] = dest_type
       else:
-        for i in range(nf):
-          d[f"maskedoff{i}"] = dest_type
+        if is_seg_load_store_tuple_type:
+          d["maskedoff_tuple"] = f"{dest_type.split('_')[0]}x{nf}_t"
+        else:
+          for i in range(nf):
+            d[f"maskedoff{i}"] = dest_type
       return d
     return {}
 
