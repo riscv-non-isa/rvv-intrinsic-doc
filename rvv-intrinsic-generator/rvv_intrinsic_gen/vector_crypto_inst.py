f3f34d32ff3051a1d7cb75181c6fe72e63516d25
eopXD <yueh.ting.chen@gmail.com>
2023-06-01 03:36:39 -0700

    [vector-crypto] Define intrinsics for the Zvbb extension
    Signed-off-by: eop Chen <eop.chen@sifive.com>

---
+++ rvv-intrinsic-generator/rvv_intrinsic_gen/vector_crypto_inst.py
@@ -0,0 +1,65 @@
+"""
+Declares the vector crypto intrinsics through the vector crypto template.
+"""
+
+from intrinsic_decorator import IntrinsicDecorators
+from templates import vector_crypto_template
+from constants import LMULS, WLMULS, SEWS, WSEWS, UITYPE
+
+
+def gen(g):
+  decorators = IntrinsicDecorators(g.has_tail_policy)
+
+  g.start_group("Zvbb - Vector Bit-manipulation used in Cryptography")
+
+  g.function_group(
+      vector_crypto_template,
+      "Vector Bit-manipulation used in Cryptography - Bitwise And-Not",
+      "",  # FIXME: We probably have a separate document for vector-crypto
+      ["vandn"],
+      UITYPE,
+      SEWS,
+      LMULS,
+      decorators.has_masking_maskedoff_policy)
+
+  g.function_group(
+      vector_crypto_template,
+      "Vector Bit-manipulation used in Cryptography - Reverse Bits",
+      "",  # FIXME: We probably have a separate document for vector-crypto
+      ["vbrev", "vbrev8", "vrev8"],
+      UITYPE,
+      SEWS,
+      LMULS,
+      decorators.has_masking_maskedoff_policy)
+
+  g.function_group(
+      vector_crypto_template,
+      "Vector Bit-manipulation used in Cryptography - Count Bits",
+      "",  # FIXME: We probably have a separate document for vector-crypto
+      ["vclz", "vctz"],
+      UITYPE,
+      SEWS,
+      LMULS,
+      decorators.has_masking_no_maskedoff)
+
+  g.function_group(
+      vector_crypto_template,
+      "Vector Bit-manipulation used in Cryptography - Rotate",
+      "",  # FIXME: We probably have a separate document for vector-crypto
+      ["vrol", "vror"],
+      UITYPE,
+      SEWS,
+      LMULS,
+      decorators.has_masking_maskedoff_policy)
+
+  g.function_group(
+      vector_crypto_template,
+      "Vector Bit-manipulation used in Cryptography - Shift",
+      "",  # FIXME: We probably have a separate document for vector-crypto
+      ["vwsll"],
+      UITYPE,
+      WSEWS,
+      WLMULS,
+      decorators.has_masking_maskedoff_policy)
+
+  ####################################################################
