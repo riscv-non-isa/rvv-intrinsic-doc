122944b87648b33c4294a6f4293898b2e97c9e26
eopXD <yueh.ting.chen@gmail.com>
2023-08-01 00:36:12 -0700

    [Auto-gen] Update tests under ../auto-generated/vector-crypto. (make git-commit-autogen-test)

--- auto-generated/vector-crypto/policy_funcs/api-testing/vaesef.c
+++ auto-generated/vector-crypto/policy_funcs/api-testing/vaesef.c
@@ -12,12 +12,40 @@ vuint32mf2_t test_vaesef_vs_u32mf2_tu(vuint32mf2_t vd, vuint32mf2_t vs2, size_t 
   return __riscv_vaesef_vs_u32mf2_tu(vd, vs2, vl);
 }
 
+vuint32m1_t test_vaesef_vs_u32m1_tu(vuint32m1_t vd, vuint32mf2_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m1_tu(vd, vs2, vl);
+}
+
+vuint32m2_t test_vaesef_vs_u32m2_tu(vuint32m2_t vd, vuint32m1_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m2_tu(vd, vs2, vl);
+}
+
+vuint32m4_t test_vaesef_vs_u32m4_tu(vuint32m4_t vd, vuint32m2_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m4_tu(vd, vs2, vl);
+}
+
+vuint32m8_t test_vaesef_vs_u32m8_tu(vuint32m8_t vd, vuint32m4_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m8_tu(vd, vs2, vl);
+}
+
 vuint32m1_t test_vaesef_vv_u32m1_tu(vuint32m1_t vd, vuint32m1_t vs2, size_t vl) {
   return __riscv_vaesef_vv_u32m1_tu(vd, vs2, vl);
 }
 
 vuint32m1_t test_vaesef_vs_u32m1_tu(vuint32m1_t vd, vuint32m1_t vs2, size_t vl) {
   return __riscv_vaesef_vs_u32m1_tu(vd, vs2, vl);
+}
+
+vuint32m2_t test_vaesef_vs_u32m2_tu(vuint32m2_t vd, vuint32m1_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m2_tu(vd, vs2, vl);
+}
+
+vuint32m4_t test_vaesef_vs_u32m4_tu(vuint32m4_t vd, vuint32m2_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m4_tu(vd, vs2, vl);
+}
+
+vuint32m8_t test_vaesef_vs_u32m8_tu(vuint32m8_t vd, vuint32m4_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m8_tu(vd, vs2, vl);
 }
 
 vuint32m2_t test_vaesef_vv_u32m2_tu(vuint32m2_t vd, vuint32m2_t vs2, size_t vl) {
@@ -28,12 +56,24 @@ vuint32m2_t test_vaesef_vs_u32m2_tu(vuint32m2_t vd, vuint32m2_t vs2, size_t vl) 
   return __riscv_vaesef_vs_u32m2_tu(vd, vs2, vl);
 }
 
+vuint32m4_t test_vaesef_vs_u32m4_tu(vuint32m4_t vd, vuint32m2_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m4_tu(vd, vs2, vl);
+}
+
+vuint32m8_t test_vaesef_vs_u32m8_tu(vuint32m8_t vd, vuint32m4_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m8_tu(vd, vs2, vl);
+}
+
 vuint32m4_t test_vaesef_vv_u32m4_tu(vuint32m4_t vd, vuint32m4_t vs2, size_t vl) {
   return __riscv_vaesef_vv_u32m4_tu(vd, vs2, vl);
 }
 
 vuint32m4_t test_vaesef_vs_u32m4_tu(vuint32m4_t vd, vuint32m4_t vs2, size_t vl) {
   return __riscv_vaesef_vs_u32m4_tu(vd, vs2, vl);
+}
+
+vuint32m8_t test_vaesef_vs_u32m8_tu(vuint32m8_t vd, vuint32m4_t vs2, size_t vl) {
+  return __riscv_vaesef_vs_u32m8_tu(vd, vs2, vl);
 }
 
 vuint32m8_t test_vaesef_vv_u32m8_tu(vuint32m8_t vd, vuint32m8_t vs2, size_t vl) {
