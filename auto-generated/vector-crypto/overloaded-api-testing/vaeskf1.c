826b646409c40f95e5fbddb34d5e90fd9c528556
eopXD <yueh.ting.chen@gmail.com>
2023-08-01 00:39:37 -0700

    [Auto-gen] Update tests under ../auto-generated/vector-crypto. (make git-commit-autogen-test)

--- auto-generated/vector-crypto/overloaded-api-testing/vaeskf1.c
+++ auto-generated/vector-crypto/overloaded-api-testing/vaeskf1.c
@@ -4,23 +4,23 @@
 typedef _Float16 float16_t;
 typedef float float32_t;
 typedef double float64_t;
-vuint32mf2_t test_vaeskf1_vi_u32mf2(vuint32mf2_t vs2, size_t uimm, size_t vl) {
+vuint32mf2_t test_vaeskf1_vi_u32mf2(vuint32mf2_t vs2, size_t vl) {
   return __riscv_vaeskf1(vs2, 0, vl);
 }
 
-vuint32m1_t test_vaeskf1_vi_u32m1(vuint32m1_t vs2, size_t uimm, size_t vl) {
+vuint32m1_t test_vaeskf1_vi_u32m1(vuint32m1_t vs2, size_t vl) {
   return __riscv_vaeskf1(vs2, 0, vl);
 }
 
-vuint32m2_t test_vaeskf1_vi_u32m2(vuint32m2_t vs2, size_t uimm, size_t vl) {
+vuint32m2_t test_vaeskf1_vi_u32m2(vuint32m2_t vs2, size_t vl) {
   return __riscv_vaeskf1(vs2, 0, vl);
 }
 
-vuint32m4_t test_vaeskf1_vi_u32m4(vuint32m4_t vs2, size_t uimm, size_t vl) {
+vuint32m4_t test_vaeskf1_vi_u32m4(vuint32m4_t vs2, size_t vl) {
   return __riscv_vaeskf1(vs2, 0, vl);
 }
 
-vuint32m8_t test_vaeskf1_vi_u32m8(vuint32m8_t vs2, size_t uimm, size_t vl) {
+vuint32m8_t test_vaeskf1_vi_u32m8(vuint32m8_t vs2, size_t vl) {
   return __riscv_vaeskf1(vs2, 0, vl);
 }
 
