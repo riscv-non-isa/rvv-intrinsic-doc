f940da09c5239b5bc4306f71f96d0a36e4b0f60c
Camel Coder <69110542+camel-cdr@users.noreply.github.com>
2023-08-09 12:09:55 +0000

    rvv_strlen.c explicitly use unsigned & unify style
    explicitly use unsigned, instead of char, which can be either signed or unsigned, which doesn't play well with the signed vector types.

--- examples/rvv_strlen.c
+++ examples/rvv_strlen.c
@@ -3,22 +3,18 @@
 #include <string.h>
 
 // reference https://github.com/riscv/riscv-v-spec/blob/master/example/strlen.s
-size_t strlen_vec(char *src) {
+size_t strlen_vec(char *source) {
   size_t vlmax = __riscv_vsetvlmax_e8m8();
-  char *copy_src = src;
+  unsigned char *src = (unsigned char*)source;
   long first_set_bit = -1;
   size_t vl;
-  while (first_set_bit < 0) {
-    vint8m8_t vec_src = __riscv_vle8ff_v_i8m8(copy_src, &vl, vlmax);
-    vbool1_t string_terminate = __riscv_vmseq_vx_i8m8_b1(vec_src, 0, vl);
-
-    copy_src += vl;
-
+  for (; first_set_bit < 0; src += vl) {
+    vuint8m8_t vec_src = __riscv_vle8ff_v_u8m8(src, &vl, vlmax);
+    vbool1_t string_terminate = __riscv_vmseq_vx_u8m8_b1(vec_src, 0, vl);
     first_set_bit = __riscv_vfirst_m_b1(string_terminate, vl);
   }
-  copy_src -= vl - first_set_bit;
-
-  return (size_t)(copy_src - src);
+  src -= vl - first_set_bit;
+  return (size_t)((char*)src - source);
 }
 
 int main() {
