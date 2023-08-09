0b7d9e18fa0634ce9ad9bdff413dc76c5fd44047
Camel Coder <69110542+camel-cdr@users.noreply.github.com>
2023-08-09 11:50:21 +0000

    rvv_memcpy.c no arithmetic on void* & fix function signature
    Resolve: #252

--- examples/rvv_memcpy.c
+++ examples/rvv_memcpy.c
@@ -2,15 +2,17 @@
 #include <riscv_vector.h>
 #include <string.h>
 
-void *memcpy_vec(void *dst, void *src, size_t n) {
-  void *save = dst;
+void *memcpy_vec(void *restrict destination, const void *restrict source,
+                 size_t n) {
+  unsigned char *dst = destination;
+  const unsigned char *src = source;
   // copy data byte by byte
   for (size_t vl; n > 0; n -= vl, src += vl, dst += vl) {
     vl = __riscv_vsetvl_e8m8(n);
     vuint8m8_t vec_src = __riscv_vle8_v_u8m8(src, vl);
     __riscv_vse8_v_u8m8(dst, vec_src, vl);
   }
-  return save;
+  return destination;
 }
 
 int main() {
