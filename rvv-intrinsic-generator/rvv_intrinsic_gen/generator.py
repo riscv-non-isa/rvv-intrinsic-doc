28b7a9d34a91409363bd0589b6ed2214c4e16015
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2024-06-07 18:00:56 +0800

    APITestGenerator: implement dummy inst_group_epilogue
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- rvv-intrinsic-generator/rvv_intrinsic_gen/generator.py
+++ rvv-intrinsic-generator/rvv_intrinsic_gen/generator.py
@@ -449,6 +449,9 @@ class APITestGenerator(Generator):
   def inst_group_prologue(self):
     return ""
 
+  def inst_group_epilogue(self):
+    return ""
+
   def write_file_header(self, has_float_type, has_bfloat16_type):
     #pylint: disable=line-too-long
     int_llvm_header = r"""// REQUIRES: riscv-registered-target
