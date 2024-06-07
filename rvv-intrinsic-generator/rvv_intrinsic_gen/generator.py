55f6f94f3a6b62a67d8ff2736ca9f790fbe24031
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2024-06-07 03:38:49 -0700

    APITestGenerator: implement dummy write function
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- rvv-intrinsic-generator/rvv_intrinsic_gen/generator.py
+++ rvv-intrinsic-generator/rvv_intrinsic_gen/generator.py
@@ -443,6 +443,9 @@ class APITestGenerator(Generator):
     # different op name
     self.test_file_names = []
 
+  def write(self, text):
+    pass
+
   def start_group(self, group_name):
     pass
 
