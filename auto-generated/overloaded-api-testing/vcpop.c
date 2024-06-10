72aac686f7027090bcffe032c78f213b3052dacf
Craig Topper <craig.topper@sifive.com>
2024-06-10 14:16:08 -0700

    Revert "[Auto-gen] Update tests under ../auto-generated. (make git-commit-autogen-test)"
    This reverts commit 26057454d3e25b1ae82b07bcbc53deda0790a1c4.

--- auto-generated/overloaded-api-testing/vcpop.c
+++ auto-generated/overloaded-api-testing/vcpop.c
@@ -1,58 +1,58 @@
 #include <riscv_vector.h>
 #include <stdint.h>
 
-unsigned int test_vcpop_m_b1(vbool1_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b1(vbool1_t vs2, size_t vl) {
   return __riscv_vcpop(vs2, vl);
 }
 
-unsigned int test_vcpop_m_b2(vbool2_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b2(vbool2_t vs2, size_t vl) {
   return __riscv_vcpop(vs2, vl);
 }
 
-unsigned int test_vcpop_m_b4(vbool4_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b4(vbool4_t vs2, size_t vl) {
   return __riscv_vcpop(vs2, vl);
 }
 
-unsigned int test_vcpop_m_b8(vbool8_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b8(vbool8_t vs2, size_t vl) {
   return __riscv_vcpop(vs2, vl);
 }
 
-unsigned int test_vcpop_m_b16(vbool16_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b16(vbool16_t vs2, size_t vl) {
   return __riscv_vcpop(vs2, vl);
 }
 
-unsigned int test_vcpop_m_b32(vbool32_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b32(vbool32_t vs2, size_t vl) {
   return __riscv_vcpop(vs2, vl);
 }
 
-unsigned int test_vcpop_m_b64(vbool64_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b64(vbool64_t vs2, size_t vl) {
   return __riscv_vcpop(vs2, vl);
 }
 
-unsigned int test_vcpop_m_b1_m(vbool1_t vm, vbool1_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b1_m(vbool1_t vm, vbool1_t vs2, size_t vl) {
   return __riscv_vcpop(vm, vs2, vl);
 }
 
-unsigned int test_vcpop_m_b2_m(vbool2_t vm, vbool2_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b2_m(vbool2_t vm, vbool2_t vs2, size_t vl) {
   return __riscv_vcpop(vm, vs2, vl);
 }
 
-unsigned int test_vcpop_m_b4_m(vbool4_t vm, vbool4_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b4_m(vbool4_t vm, vbool4_t vs2, size_t vl) {
   return __riscv_vcpop(vm, vs2, vl);
 }
 
-unsigned int test_vcpop_m_b8_m(vbool8_t vm, vbool8_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b8_m(vbool8_t vm, vbool8_t vs2, size_t vl) {
   return __riscv_vcpop(vm, vs2, vl);
 }
 
-unsigned int test_vcpop_m_b16_m(vbool16_t vm, vbool16_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b16_m(vbool16_t vm, vbool16_t vs2, size_t vl) {
   return __riscv_vcpop(vm, vs2, vl);
 }
 
-unsigned int test_vcpop_m_b32_m(vbool32_t vm, vbool32_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b32_m(vbool32_t vm, vbool32_t vs2, size_t vl) {
   return __riscv_vcpop(vm, vs2, vl);
 }
 
-unsigned int test_vcpop_m_b64_m(vbool64_t vm, vbool64_t vs2, size_t vl) {
+unsigned long test_vcpop_m_b64_m(vbool64_t vm, vbool64_t vs2, size_t vl) {
   return __riscv_vcpop(vm, vs2, vl);
 }
