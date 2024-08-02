cd7c0d44cc6d9a38e0f95304f7c7a939cc1a2733
Brandon Wu <brandon.wu@sifive.com>
2024-08-02 04:38:27 -0700

    [Auto-gen] Update bfloat16 tests under ../auto-generated. (make git-commit-autogen-bf16-test)

---
+++ auto-generated/bfloat16/policy_funcs/api-testing/vmerge.c
@@ -0,0 +1,38 @@
+#include <riscv_vector.h>
+#include <stdint.h>
+
+vbfloat16mf4_t test_vmerge_vvm_bf16mf4_tu(vbfloat16mf4_t vd, vbfloat16mf4_t vs2,
+                                          vbfloat16mf4_t vs1, vbool64_t v0,
+                                          size_t vl) {
+  return __riscv_vmerge_vvm_bf16mf4_tu(vd, vs2, vs1, v0, vl);
+}
+
+vbfloat16mf2_t test_vmerge_vvm_bf16mf2_tu(vbfloat16mf2_t vd, vbfloat16mf2_t vs2,
+                                          vbfloat16mf2_t vs1, vbool32_t v0,
+                                          size_t vl) {
+  return __riscv_vmerge_vvm_bf16mf2_tu(vd, vs2, vs1, v0, vl);
+}
+
+vbfloat16m1_t test_vmerge_vvm_bf16m1_tu(vbfloat16m1_t vd, vbfloat16m1_t vs2,
+                                        vbfloat16m1_t vs1, vbool16_t v0,
+                                        size_t vl) {
+  return __riscv_vmerge_vvm_bf16m1_tu(vd, vs2, vs1, v0, vl);
+}
+
+vbfloat16m2_t test_vmerge_vvm_bf16m2_tu(vbfloat16m2_t vd, vbfloat16m2_t vs2,
+                                        vbfloat16m2_t vs1, vbool8_t v0,
+                                        size_t vl) {
+  return __riscv_vmerge_vvm_bf16m2_tu(vd, vs2, vs1, v0, vl);
+}
+
+vbfloat16m4_t test_vmerge_vvm_bf16m4_tu(vbfloat16m4_t vd, vbfloat16m4_t vs2,
+                                        vbfloat16m4_t vs1, vbool4_t v0,
+                                        size_t vl) {
+  return __riscv_vmerge_vvm_bf16m4_tu(vd, vs2, vs1, v0, vl);
+}
+
+vbfloat16m8_t test_vmerge_vvm_bf16m8_tu(vbfloat16m8_t vd, vbfloat16m8_t vs2,
+                                        vbfloat16m8_t vs1, vbool2_t v0,
+                                        size_t vl) {
+  return __riscv_vmerge_vvm_bf16m8_tu(vd, vs2, vs1, v0, vl);
+}
