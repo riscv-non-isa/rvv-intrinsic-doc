4249c4a3720ecf1d507ca9b84181f5fe250ada86
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2024-10-01 15:33:28 +0800

    [Auto-gen] Update bfloat16 documents under ../auto-generated. (make git-commit-autogen-bf16-doc)

--- auto-generated/bfloat16/policy_funcs/llvm-api-tests/vfmv.c
+++ auto-generated/bfloat16/policy_funcs/llvm-api-tests/vfmv.c
@@ -1,17 +1,19 @@
 // REQUIRES: riscv-registered-target
 // RUN: %clang_cc1 -triple riscv64 -target-feature +v \
-// RUN:   -target-feature +experimental-zvfbfmin \
-// RUN:   -target-feature +experimental-zvfbfwma -disable-O0-optnone \
+// RUN:   -target-feature +zvfbfmin \
+// RUN:   -target-feature +zvfbfwma -disable-O0-optnone \
 // RUN:   -emit-llvm %s -o - | opt -S -passes=mem2reg | \
 // RUN:   FileCheck --check-prefix=CHECK-RV64 %s
 
 #include <riscv_vector.h>
 
-vbfloat16mf4_t test_vfmv_v_f_bf16mf4_tu(vbfloat16mf4_t vd, __bf16 rs1, size_t vl) {
+vbfloat16mf4_t test_vfmv_v_f_bf16mf4_tu(vbfloat16mf4_t vd, __bf16 rs1,
+                                        size_t vl) {
   return __riscv_vfmv_v_f_bf16mf4_tu(vd, rs1, vl);
 }
 
-vbfloat16mf2_t test_vfmv_v_f_bf16mf2_tu(vbfloat16mf2_t vd, __bf16 rs1, size_t vl) {
+vbfloat16mf2_t test_vfmv_v_f_bf16mf2_tu(vbfloat16mf2_t vd, __bf16 rs1,
+                                        size_t vl) {
   return __riscv_vfmv_v_f_bf16mf2_tu(vd, rs1, vl);
 }
 
