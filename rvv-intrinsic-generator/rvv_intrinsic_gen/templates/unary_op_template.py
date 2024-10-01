e3cf549026fe9e6d31733d851c4611761d95e84b
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2024-10-01 15:31:41 +0800

    template/unary_op: add op assertion to address type-check failure
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- rvv-intrinsic-generator/rvv_intrinsic_gen/templates/unary_op_template.py
+++ rvv-intrinsic-generator/rvv_intrinsic_gen/templates/unary_op_template.py
@@ -43,6 +43,7 @@ def render(G,
   for decorator in decorator_list:
     decorator.write_text_header(G)
     for args in prod(OP=op_list, TYPE=type_list, SEW=sew_list, LMUL=lmul_list):
+      assert args["OP"] is not None
       data_type = args["TYPE"]
       op = args["OP"]
 
