36723fb10214501cbf6cc8f314519773f14323bd
Jim Lin <jim@andestech.com>
2025-07-11 13:50:07 +0800

    [Auto-gen] Update bfloat16 tests under ../auto-generated. (make git-commit-autogen-bf16-test)

---
+++ auto-generated/bfloat16/policy_funcs/overloaded-api-testing/vcompress.c
@@ -0,0 +1,34 @@
+#include <riscv_vector.h>
+#include <stdint.h>
+
+vbfloat16mf4_t test_vcompress_vm_bf16mf4_tu(vbfloat16mf4_t vd,
+                                            vbfloat16mf4_t vs2, vbool64_t vs1,
+                                            size_t vl) {
+  return __riscv_vcompress_tu(vd, vs2, vs1, vl);
+}
+
+vbfloat16mf2_t test_vcompress_vm_bf16mf2_tu(vbfloat16mf2_t vd,
+                                            vbfloat16mf2_t vs2, vbool32_t vs1,
+                                            size_t vl) {
+  return __riscv_vcompress_tu(vd, vs2, vs1, vl);
+}
+
+vbfloat16m1_t test_vcompress_vm_bf16m1_tu(vbfloat16m1_t vd, vbfloat16m1_t vs2,
+                                          vbool16_t vs1, size_t vl) {
+  return __riscv_vcompress_tu(vd, vs2, vs1, vl);
+}
+
+vbfloat16m2_t test_vcompress_vm_bf16m2_tu(vbfloat16m2_t vd, vbfloat16m2_t vs2,
+                                          vbool8_t vs1, size_t vl) {
+  return __riscv_vcompress_tu(vd, vs2, vs1, vl);
+}
+
+vbfloat16m4_t test_vcompress_vm_bf16m4_tu(vbfloat16m4_t vd, vbfloat16m4_t vs2,
+                                          vbool4_t vs1, size_t vl) {
+  return __riscv_vcompress_tu(vd, vs2, vs1, vl);
+}
+
+vbfloat16m8_t test_vcompress_vm_bf16m8_tu(vbfloat16m8_t vd, vbfloat16m8_t vs2,
+                                          vbool2_t vs1, size_t vl) {
+  return __riscv_vcompress_tu(vd, vs2, vs1, vl);
+}
